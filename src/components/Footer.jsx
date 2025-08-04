a1c6fcc3b4a6db2940dfabf5b7ce31b0343d29bb
Damien <dtriegaardt@outlook.com>
2025-08-04 20:49:05 +1000

    Update styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 function Footer() {
   return (
     <footer>
+      <div className="bgGradient" />
       <div>
         <h2>The Brutal Martian Landscape</h2>
         <h1>APOD PROJECT</h1>
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,11 +1,17 @@
 function SideBar() {
   return (
-    <div>
-      <h2>The Brutal Martian Landscape</h2>
-      <div>
-        <p>Description</p>
-        <p>Lorem ipsum</p>
+    <div className="sidebar">
+      <div className="bgOverlay" />
+      <div className="sidebarContents">
+        <h2>The Brutal Martian Landscape</h2>
+        <div>
+          <p>Description</p>
+          <p>Lorem ipsum</p>
+        </div>
       </div>
+      <button type="button" aria-label="Close Sidebar" onClick={() => {}}>
+        <i className="fa-solid fa-arrow-right" />
+      </button>
     </div>
   );
 }
