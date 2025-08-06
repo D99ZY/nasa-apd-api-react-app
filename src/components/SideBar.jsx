e1ab51509ea15c4831206c7cd51b340d0a58cabf
Damien <dtriegaardt@outlook.com>
2025-08-06 20:30:49 +1000

    Update style

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -7,20 +7,20 @@ function SideBar(props) {
       <div className="bgOverlay" />
       <div className="sidebarContents">
         <h2>The Brutal Martian Landscape</h2>
-        <div>
-          <p>Description</p>
+        <div className="descriptionContainer">
+          <p className="descriptionTitle">Description</p>
           <p>Lorem ipsum</p>
         </div>
+        <button
+          type="button"
+          aria-label="Close Sidebar"
+          onClick={() => {
+            handleToggleModal();
+          }}
+        >
+          <i className="fa-solid fa-arrow-right" />
+        </button>
       </div>
-      <button
-        type="button"
-        aria-label="Close Sidebar"
-        onClick={() => {
-          handleToggleModal();
-        }}
-      >
-        <i className="fa-solid fa-arrow-right" />
-      </button>
     </div>
   );
 }
