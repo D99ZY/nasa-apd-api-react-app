f3dec36bf1df7d37dbe2abb55eb9185f99db3be0
Damien <dtriegaardt@outlook.com>
2025-08-08 21:06:02 +1000

    update alt attribute in img tag

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,7 +4,7 @@ function Main(props) {
   const { data } = props;
   return (
     <div className="imgContainer">
-      <img src={data.hdurl} alt={data.title} className="bgImage" />;
+      <img src={data.hdurl} alt={data.title || 'bg-img'} className="bgImage" />;
     </div>
   );
 }
