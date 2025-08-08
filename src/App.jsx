734dda3942dda18e07e7f4ad0adad4e5a1fb37b0
Damien <dtriegaardt@outlook.com>
2025-08-08 21:11:47 +1000

    Add comments

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,21 @@ function App() {
   const [data, setData] = useState(null);
   const [showModal, setShowModal] = useState(false);
 
+  // Toggle hide / show side bar modal
   const handleToggleModal = useCallback(() => {
     setShowModal(!showModal);
   }, [showModal]);
 
+  // Set data on page load
   useEffect(() => {
+    // Retrieve NASA API key from .env file
     const NASA_KEY = import.meta.env.VITE_NASA_API_KEY;
+
+    // Retrieve and set data
     async function fetchAPIData() {
       const url = `https://api.nasa.gov/planetary/apod?api_key=${NASA_KEY}`;
 
+      // Set data with local storage
       const today = new Date().toDateString();
       const localKey = `NASA-${today}`;
       if (localStorage.getItem(localKey)) {
@@ -23,6 +29,8 @@ function App() {
         setData(apiData);
         return;
       }
+
+      // Set data with API call
       localStorage.clear();
       try {
         const res = await fetch(url);
